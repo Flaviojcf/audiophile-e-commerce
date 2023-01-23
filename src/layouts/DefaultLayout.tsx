9be9c053c7ece1365d2e384dec54cd4182b9b970
Flavio <fjcf@poli.br>
2023-01-23 18:38:49 -0300

    refactor: change default layout to checkout route

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,18 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { BringingAudio } from "../components/bringingAudio/BringingAudio";
 import { Footer } from "../components/footer/Footer";
 import { Header } from "../components/header/Header";
 
+export function DefaultLayout() {
+  const { pathname } = useLocation();
 
-export function DefaultLayout(){
-    return(
-        <div>
-            <Header/>
-            <Outlet/>
-            <div className="flex items-center justify-center mb-48 px-40 tablet:px-10 mobile:px-6 ">
-            <BringingAudio/>
-            </div>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <div
+        className={
+          pathname === "/checkout"
+            ? "hidden"
+            : "flex items-center justify-center mb-48 px-40 tablet:px-10 mobile:px-6 "
+        }
+      >
+        <BringingAudio />
+      </div>
+      <Footer />
+    </div>
+  );
 }
